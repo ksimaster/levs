e3956810fc7f1453bd408ba4c5577e4787324bac
ksimaster <ksimasterlord@gmail.com>
2022-10-27 20:26:36 +0300

    fix restart

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -25,7 +25,7 @@ public class GameController : MonoBehaviour
     }
     public void Restart()
     {
-        SceneManager.LoadScene("MainScene");
+        SceneManager.LoadScene("MainScene_EN");
     }
     public void PauseOn()
     {
